1f3cbde815a56e39bd1de23298f647a76d729a78
BRITVA-hp <borbritvahp@gmail.com>
2022-04-26 22:16:47 +0400

    video

--- js/script.js
+++ js/script.js
@@ -64,4 +64,187 @@ document.addEventListener('DOMContentLoaded', () => {
 
     modal('.modal-main', 'modal--active', '[data-modal]', '.modal-main__close');
 
+              // Функция слайдера
+              function slider(window, field, cards, cardWidth, margin, dotsWrap, dotClass, dotClassActive, arrowPrev, arrowNext, arrowClass, sliderName, sliderSlideName) {
+                const window_ = document.querySelector(window),
+                    field_ = document.querySelector(field),
+                    cards_ = document.querySelectorAll(cards),
+                    arrowPrev_ = document.querySelector(arrowPrev),
+                    arrowNext_ = document.querySelector(arrowNext),
+                    sliderName_ = document.querySelector(sliderName),
+                    sliderSlideName_ = document.querySelectorAll(sliderSlideName);
+        
+                let startPoint,
+                    swipeAction,
+                    endPoint,
+                    sliderCounter = 0,
+                    dots_ = [];
+        
+                // Устанавливаем фиксированную ширину поля слайдов
+        
+                field_.style.width = `${cardWidth * cards_.length + (margin * (cards_.length - 1))}px`;
+                field_.style.marginLeft = '20px';
+                field_.style.marginRight = '20px';
+        
+                // Слайд следующий
+        
+                function slideNext() {
+                    sliderCounter++;
+                    arrowNext_.classList.remove(arrowClass);
+                    arrowPrev_.classList.remove(arrowClass);
+                    if (sliderCounter >= cards_.length) {
+                        sliderCounter = cards_.length - 1;
+                    }
+                    if ((sliderCounter + 1) == cards_.length) {
+                        arrowNext_.classList.add(arrowClass);
+                    }
+                    if (dotsWrap) {
+                        dots_.forEach((item, index)=> {
+                        item.classList.remove(dotClassActive);
+                        if (index == sliderCounter) {
+                            item.classList.add(dotClassActive);
+                        }
+                        });
+                    }
+                    field_.style.transform = `translateX(-${(cardWidth + margin) * sliderCounter}px)`;
+                    if (sliderName_) {
+                    }
+                }
+        
+                // Слайд предыдущий
+        
+                function slidePrev() {
+                    sliderCounter--;
+                    arrowNext_.classList.remove(arrowClass);
+                    arrowPrev_.classList.remove(arrowClass);
+                    if (sliderCounter <= 0) {
+                        sliderCounter = 0;
+                    }
+                    if (sliderCounter == 0) {
+                        arrowPrev_.classList.add(arrowClass);
+                    }
+                    if (dotsWrap) {
+                        dots_.forEach((item, index)=> {
+                            item.classList.remove(dotClassActive);
+                            if (index == sliderCounter) {
+                                item.classList.add(dotClassActive);
+                            }
+                        });
+                    }
+                    field_.style.transform = `translateX(-${(cardWidth + margin) * sliderCounter}px)`;
+                    if (sliderName_) {
+                    }
+                }
+        
+                // Рендер точек
+        
+                if (dotsWrap) {
+                    const dotsWrap_ = document.querySelector(dotsWrap);
+            
+                    cards_.forEach(() => {
+                        const dot = document.createElement('div');
+                        dot.classList.add(dotClass);
+                        dotsWrap_.appendChild(dot);
+                        dots_.push(dot);
+                    });
+                    dots_[0].classList.add(dotClassActive);
+                    dots_.forEach((item, index) => {
+                        item.addEventListener('click', () => {
+                        sliderCounter = index;
+                        arrowNext_.classList.remove(arrowClass);
+                        arrowPrev_.classList.remove(arrowClass);
+                        if (sliderCounter == 0) {
+                            arrowPrev_.classList.add(arrowClass);
+                        }
+                        if ((sliderCounter + 1) == cards_.length) {
+                            arrowNext_.classList.add(arrowClass);
+                        }
+                        dots_.forEach(item_ => {
+                            item_.classList.remove(dotClassActive);
+                        });
+                        item.classList.add(dotClassActive);
+                        field_.style.transform = `translateX(-${(cardWidth + margin) * sliderCounter}px)`;
+                        });
+                    });
+                }
+        
+                // Переключение на стрелки
+        
+                arrowPrev_.addEventListener('click', () => {
+                    slidePrev();
+                });
+        
+                arrowNext_.addEventListener('click', () => {
+                    slideNext();
+                });
+        
+                // Свайп слайдов тач-событиями
+        
+                window_.addEventListener('touchstart', (e) => {
+                    startPoint = e.changedTouches[0].pageX;
+                });
+        
+                window_.addEventListener('touchmove', (e) => {
+                    swipeAction = e.changedTouches[0].pageX - startPoint;
+                    field_.style.transform = `translateX(${swipeAction + (-(cardWidth + margin) * sliderCounter)}px)`;
+                });
+        
+                window_.addEventListener('touchend', (e) => {
+                    endPoint = e.changedTouches[0].pageX;
+                    if (Math.abs(startPoint - endPoint) > 50) {
+                        arrowNext_.classList.remove(arrowClass);
+                        arrowPrev_.classList.remove(arrowClass);
+                        if (endPoint < startPoint) {
+                            slideNext();
+                        } else {
+                            slidePrev();
+                        }
+                    } else {
+                        field_.style.transform = `translateX(-${(cardWidth + margin) * sliderCounter}px)`;
+                    }
+                });
+            }
+        
+            slider(
+                '.cons__window--desc',
+                '.cons__field--desc',
+                '.cons__card--desc',
+                350,
+                20,
+                false,
+                false,
+                false,
+                '.cons__arrow--prev',
+                '.cons__arrow--next',
+                'cons__arrow--inactive'
+            );
+
+    // Видео
+
+    function video() {
+        const play = document.querySelectorAll('.cons__play'),
+            video = document.querySelector('#video'),
+            modalVideo = document.querySelector('.modal-video');
+
+        play.forEach(item => {
+            item.addEventListener('click', (e) => {
+                const path = e.currentTarget.getAttribute('data-video');
+                modalVideo.classList.add('modal--active');
+                document.body.style.overflow = 'hidden';
+                video.setAttribute('src', path);
+                video.play();
+            });
+        });
+
+        modalVideo.addEventListener('click', (e) => {
+            if (e.target.classList.contains('modal--active')) {
+                modalVideo.classList.remove('modal--active');
+                video.pause();
+                document.body.style.overflow = '';
+            }
+        });
+    }
+
+    video();
+
 });
